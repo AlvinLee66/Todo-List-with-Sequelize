fd77374e6488916835e64a940c7c0466ab19bbfe
AlvinLee66 <ewqaz001@gmail.com>
2022-07-03 00:06:49 +0800

    feat: add facebook login & dotenv

--- app.js
+++ app.js
@@ -4,9 +4,14 @@ const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const flash = require('connect-flash')
 const usePassport = require('./config/passport')
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const routes = require('./routes')
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
