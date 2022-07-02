35635c06eb7fb9a60755356095c43be3023215a1
AlvinLee66 <ewqaz001@gmail.com>
2022-07-02 20:12:30 +0800

    feat: add passport-local function on login

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
+const session = require('express-session')
 const bcrypt = require('bcryptjs')
+const usePassport = require('./config/passport')
 const app = express()
 const PORT = 3000
 const db = require('./models')
 const Todo = db.Todo
 const User = db.User
+const passport = require('passport')
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+app.use(session({
+secret: 'ThisIsMySecret',
+resave: false,
+  saveUninitialized: true
+}))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+usePassport(app)
 
 app.get('/', (req, res) => {
   return Todo.findAll({
@@ -26,9 +35,10 @@ app.get('/users/login', (req, res) => {
   res.render('login')
 })
 
-app.post('/users/login', (req, res) => {
-  res.send('login')
-})
+app.post('/users/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/users/login'
+}))
 
 app.get('/users/register', (req, res) => {
   res.render('register')
@@ -61,7 +71,7 @@ app.post('/users/register', (req, res) => {
 })
 
 app.get('/users/logout', (req, res) => {
-  res.send('logout')
+  res.redirect('/users/login')
 })
 
 app.get('/todos/:id', (req, res) => {
